28f3fbaecefa08afcbd10925ef48ce6688141c95
Yumna <yumnamubeen0019@gmail.com>
2024-09-06 13:08:03 +0500

    Add Blog Form

--- src/components/CreateBlog.js
+++ src/components/CreateBlog.js
@@ -2,6 +2,18 @@ const CreateBlog = () => {
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog Title: </label>
+                <input type="text" required />
+                <label>Blog Body: </label>
+                <textarea required></textarea>
+                <label>Blog Author: </label>
+                <select >
+                    <option value="mario">Student</option>
+                    <option value="yoshi">Professor</option>
+                </select>
+            </form>
+            <button>Add Blog</button>
         </div>
      );
 }
